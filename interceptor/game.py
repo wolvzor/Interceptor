b46d168a20964a89353109ee774b28ea40bb617b
wolvzor <wolvie@wolviegames.com>
2020-05-08 12:48:31 -0600

    Added more ship graphics.

--- interceptor/game.py
+++ interceptor/game.py
@@ -53,12 +53,20 @@ hexes = hexgrid.generate_points()
 renegade_pilot = Pilot("Jane", "Minerva", 5, 4, renegade_fighters['Cheetah'])
 renegade_pilot.change_heading(0)
 renegade_pilot.change_hex(4,3)
-renegade_pilots = [renegade_pilot]
+
+renegade_pilot2 = Pilot("Jane", "Minerva", 5, 4, renegade_fighters['Guardian'])
+renegade_pilot2.change_heading(2)
+renegade_pilot2.change_hex(3,2)
+renegade_pilots = [renegade_pilot, renegade_pilot2]
 
 tog_pilot = Pilot("John", "Invictus", 6, 6, tog_fighters['Lancea'])
 tog_pilot.change_heading(4)
 tog_pilot.change_hex(2,2)
-tog_pilots = [tog_pilot]
+
+tog_pilot2 = Pilot("John", "Invictus", 6, 6, tog_fighters['Verutum'])
+tog_pilot2.change_heading(3)
+tog_pilot2.change_hex(1,0)
+tog_pilots = [tog_pilot, tog_pilot2]
 
 while True:
 
